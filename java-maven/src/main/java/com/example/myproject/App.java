d1c3cb61f061890f93a7ee1ff239524b328c5aef
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-09-01 15:48:35 +0200

    Update App.java

--- java-maven/src/main/java/com/example/myproject/App.java
+++ java-maven/src/main/java/com/example/myproject/App.java
@@ -10,8 +10,8 @@ public class App {
 
   public static int compare(int a, int b) {
     System.out.println("THIS IS A JAVA SYSTEM OUT PRINTLN FROM BAZEL TEST");
-    System.out.println("##teamcity[buildProblem flowId='20' description='Test printTCMessage failed chubatova']");
-    System.out.println("##teamcity[buildStatus status='SUCCESS' text='test_status_chubatova']");
+    //System.out.println("##teamcity[buildProblem flowId='20' description='Test printTCMessage failed chubatova']");
+    //System.out.println("##teamcity[buildStatus status='SUCCESS' text='test_status_chubatova']");
     System.out.println("##teamcity[publishArtifacts '+:**/* => dir40step2']");
     
     return Ints.compare(a, b);
