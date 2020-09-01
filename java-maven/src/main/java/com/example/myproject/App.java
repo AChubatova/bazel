43ca878e7cbacf4ac021a34e37cf00512303002f
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-09-01 15:29:29 +0200

    Update App.java

--- java-maven/src/main/java/com/example/myproject/App.java
+++ java-maven/src/main/java/com/example/myproject/App.java
@@ -10,6 +10,7 @@ public class App {
 
   public static int compare(int a, int b) {
     System.out.println("THIS IS A JAVA SYSTEM OUT PRINTLN FROM BAZEL TEST");
+    System.out.println("##teamcity[buildProblem flowId='20' description='Test printTCMessage failed chubatova']");
     return Ints.compare(a, b);
 
   }
