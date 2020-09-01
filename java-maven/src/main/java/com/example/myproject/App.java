2dd2a88ec66738d444bdfbc35e0d1bcd244b63ff
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-09-01 15:46:37 +0200

    Update App.java

--- java-maven/src/main/java/com/example/myproject/App.java
+++ java-maven/src/main/java/com/example/myproject/App.java
@@ -12,6 +12,8 @@ public class App {
     System.out.println("THIS IS A JAVA SYSTEM OUT PRINTLN FROM BAZEL TEST");
     System.out.println("##teamcity[buildProblem flowId='20' description='Test printTCMessage failed chubatova']");
     System.out.println("##teamcity[buildStatus status='SUCCESS' text='test_status_chubatova']");
+    "##teamcity[publishArtifacts '+:**/* => dir40step2']"
+    
     return Ints.compare(a, b);
 
   }
